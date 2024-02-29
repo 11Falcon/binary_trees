cca7e8725d370311d094fd2b7ea414b5d2fc58e5
Falcon <soufian9mkns@gmail.com>
2024-02-28 23:52:40 -0800

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -12,21 +12,21 @@ int binary_tree_balance(const binary_tree_t *tree)
 	if (tree->left && !tree->left->right && !tree->left->left)
 		left = 1;
 	else
-		left = binary_tree_heigh(tree->left);
+		left = binary_tree_height(tree->left);
 	if (tree->right && !tree->right->left && !tree->right->right)
 		right = 1;
 	else
-		right = binary_tree_heigh(tree->right);
+		right = binary_tree_height(tree->right);
 	return (left - right);
 }
-int binary_tree_heigh(const binary_tree_t *tree)
+size_t binary_tree_height(const binary_tree_t *tree)
 {
         if (!tree)
                 return (0);
         if (!tree->right && !tree->left)
                 return (0);
-        if (binary_tree_heigh(tree->right) < binary_tree_heigh(tree->left))
-                return (binary_tree_heigh(tree->left) + 1);
+        if (binary_tree_height(tree->right) < binary_tree_height(tree->left))
+                return (binary_tree_height(tree->left) + 1);
         else
-                return (binary_tree_heigh(tree->right) + 1);
+                return (binary_tree_height(tree->right) + 1);
 }
